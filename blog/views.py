6d4b7e463c8157baaf30ca7929bfc6b5e2276c86
Zelin Zhu <3088451Z@student.gla.ac.uk>
2026-03-09 08:48:42 +0000

    Add post_list view and base template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(status='published').order_by('-create_time')
+    context = {'posts': posts}
+    return render(request, 'blog/post_list.html', context=context)
