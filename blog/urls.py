40c3fedfae3c70fd23dab444b821950924b0601a
Zelin Zhu <3088451Z@student.gla.ac.uk>
2026-03-09 09:21:32 +0000

    Add post_detail view and URL

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     #the url to post_list
     path('', views.post_list, name='post_list'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -7,3 +7,8 @@ def post_list(request):
     posts = Post.objects.filter(status='published').order_by('-create_time')
     context = {'posts': posts}
     return render(request, 'blog/post_list.html', context=context)
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context=context)
