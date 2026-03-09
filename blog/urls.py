5957dbc5bae8323a1eba2b7587a7b38a092421a3
Zelin Zhu <3088451Z@student.gla.ac.uk>
2026-03-09 21:00:52 +0000

    edit post function done

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('create/', views.create_post, name='create_post'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .forms import CreatePostForm
 from .models import Post
+from django.http import HttpResponseForbidden
 # Create your views here.
 
 def post_list(request):
@@ -32,3 +33,23 @@ def create_post(request):
     else:
         form = CreatePostForm()
     return render(request, 'blog/create_post.html', context={'form': form})
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if post.author != request.user:
+        return HttpResponseForbidden("You are not allowed to edit this post.")
+    if request.method == 'POST':
+        form = CreatePostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            if 'save_draft' in request.POST:
+                post.status = 'draft'
+            else:
+                post.status = 'published'
+            post.save()
+        return redirect('blog:post_list')
+    else:
+        form = CreatePostForm(instance=post)
+    return render(request, 'blog/edit_post.html', context={'form': form})
